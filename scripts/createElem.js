95878126eda72fb827caca93ae86138bb1d72351
drvicx <nuzhdin.vic@gmail.com>
2022-06-18 20:12:53 +0600

    Added createElem() func

---
+++ scripts/createElem.js
@@ -0,0 +1,28 @@
+/**
+ *=CREATES DOM-ELEMENT/OBJECT BY TAG-NAME AND ATTRIBUTES (key:value);
+ * @param {*} tag   - html tag name;
+ * @param {*} attrs - key:value field data (attributes data);
+ */
+export const createElem = (tag, attrs) => {
+
+    //=Create empty DOM-element (object)
+    const elem = document.createElement(tag);
+    //=Map attributes to element
+    Object.assign(elem, attrs);
+    
+    //--DEBUG: print element
+    //console.log('elem: ', elem);
+
+    return elem;
+
+}
+
+//--DEBUG: test createElement() func
+/*
+createElem('div', {
+    className: 'block',
+    textContent: 'test content',
+    title: 'test title'
+
+});
+*/
