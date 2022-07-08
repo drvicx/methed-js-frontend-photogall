6788967cf92dc6efd73fe8f3c584cd918b0606b8
drvicx <nuzhdin.vic@gmail.com>
2022-07-08 11:31:05 +0600

    New pagination logic

--- scripts/scrollLoad.js
+++ scripts/scrollLoad.js
@@ -11,10 +11,14 @@ import { createCardPhoto } from './createCardPhoto.js';
  * 
  */
 export const scrollLoad = (gallery, masonryGrid, terminator) => {
+
+    //=Initial Page counter
+    let i = 1;
+
+    //=Using IntesectionObserver for subscribe terminator-element load event
     const observer = new IntersectionObserver(
         //--1st argument: element(s) for Observer subscribing
-
-        //--TEST1: show console message when last terminator element is rendered
+        //--DEBUG: show console message terminator-element is loaded at the end of the page
         /*
         (entries) => {
             //-Checks if terminator element is rendered on index.html when scrolling
@@ -25,20 +29,21 @@ export const scrollLoad = (gallery, masonryGrid, terminator) => {
         },
         */
 
-        //--TEST2: render pseudo-new cards when last terminator element is rendered
+        //--1st argument: element(s) for Observer subscribing
+        //=Get new portion of cards when terminator-element is loaded at the end of the page
         async (entries) => {
-            //-Checks if terminator element is rendered on index.html when scrolling
+            //..Checks if terminator element is rendered on index.html when scrolling
             if (entries[0].isIntersecting) {
-                //=Asynchronously Get array of JavaScript Objects (photos data from data.json)
-                const photos = await getData();
-                //=Getting array of 30 <li> DOM-elements from createCardPhoto() funct and map them to "cards" array
+                //..Asynchronously Get array of JavaScript Objects (photos data from data.json)
+                const photos = await getData({ page: ++i, count: 30 });
+                //..Getting array of 30 <li> DOM-elements from createCardPhoto() funct and map them to "cards" array
                 const cards = photos.map(createCardPhoto);
-                //=Asynchronously Create parent <ul> element with photos <li> elements selected with .grid class
-                // Waits when all Promises will be fullfilled and render Masonry grid with photo-cards
+                //..Asynchronously Create parent <ul> element with photos <li> elements selected with .grid class
+                //  Waits when all Promises will be fullfilled and render Masonry grid with photo-cards
                 Promise.all(cards).then(cards => {
-                    //=Add child <li> elements to parent <ul> selected with .grid class
+                    //..Add child <li> elements to parent <ul> selected with .grid class
                     gallery.append(...cards);
-                    //=Apply Masonry-grid to Photo Cards - place/append Array of <li> card elements into Masonry grid Object
+                    //..Apply Masonry-grid to Photo Cards - place/append Array of <li> card elements into Masonry grid Object
                     masonryGrid.appended(cards);
                 })
             } 
