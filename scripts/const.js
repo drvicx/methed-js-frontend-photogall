c7a295e74233cc3bb9292ed38d2be0ec9333072c
drvicx <nuzhdin.vic@gmail.com>
2022-07-15 13:48:36 +0600

    Change Dev to Prod App endpoint

--- scripts/const.js
+++ scripts/const.js
@@ -7,7 +7,8 @@ export const API_SECRET_KEY = '-W3HVrthIEoK45KPVgKemqELQWcaS9ZY5jewDDaG07s';
 //--Unsplash Authorization workflow (OAuth2 protocol) constants
 export const OAUTH_AUTH_URL = 'https://unsplash.com/oauth/authorize';
 export const OAUTH_CLIENT_ID = API_ACCESS_KEY;
-export const OAUTH_REDIRECT_URI = 'http://127.0.0.1:5500/';
+//export const OAUTH_REDIRECT_URI = 'http://127.0.0.1:5500/';                                 //--DEV
+export const OAUTH_REDIRECT_URI = 'https://drvicx.github.io/methed-js-frontend-photogall/';   //--PROD GitHub Pages
 export const OAUTH_RESPONSE_TYPE = 'code';
 export const OAUTH_PERM_SCOPE = 'public read_photos read_user write_likes';
 export const OAUTH_REQUEST_ACCESS_TOKEN_URL = 'https://unsplash.com/oauth/token';
